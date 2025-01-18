96dfbcdf8de0398b831868687cad31e6d0d85db8
silver-will <99222613+Silver-will@users.noreply.github.com>
2025-01-18 02:17:41 +0100

    Implemented Image based lighting

--- Black_Key/src/graphics.h
+++ Black_Key/src/graphics.h
@@ -6,7 +6,7 @@ class VulkanEngine;
 
 namespace black_key {
 	bool is_visible(const RenderObject& obj, const glm::mat4& viewproj);
-	void generate_brdf_flut(VulkanEngine* engine);
+	void generate_brdf_lut(VulkanEngine* engine);
 	void generate_irradiance_cube(VulkanEngine* engine);
 	void generate_prefiltered_cubemap(VulkanEngine* engine);
 }
