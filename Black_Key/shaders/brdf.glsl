02ce8d29e22a82a4082c1a32273f2e49081f3d8c
silver-will <99222613+Silver-will@users.noreply.github.com>
2025-07-31 09:43:05 +0100

    PBR testing

---
+++ Black_Key/shaders/brdf.glsl
@@ -0,0 +1,164 @@
+#include "resource.glsl"
+
+#define MEDIUMP_FLT_MAX    65504.0
+#define saturateMediump(x) min(x, MEDIUMP_FLT_MAX)
+const float PI = 3.14159265359;
+
+float V_SmithGGXCorrelated(float NoV, float NoL, float roughness);
+float D_Burley(float roughness, float LoH, float NoL, float NoV);
+float D_GGX_IMPROVED(float roughness, float NoH, const vec3 n, const vec3 h);
+
+
+vec3 prefilteredReflection(vec3 R, float roughness)
+{
+	const float MAX_REFLECTION_LOD = 9.0; // todo: param/const
+	float lod = roughness * MAX_REFLECTION_LOD;
+	float lodf = floor(lod);
+	float lodc = ceil(lod);
+	vec3 a = textureLod(preFilterMap, R, lodf).rgb;
+	vec3 b = textureLod(preFilterMap, R, lodc).rgb;
+	return mix(a, b, lod - lodf);
+}
+
+// ----------------------------------------------------------------------------
+vec3 fresnelSchlick(float cosTheta, vec3 F0)
+{
+    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+}
+
+float F_Schlick(float u, float f0, float f90) {
+    return f0 + (f90 - f0) * pow(1.0 - u, 5.0);
+}
+
+vec3 EvaluateIBL(vec3 N, vec3 diffuseColor, vec3 f0, vec3 f90, float perceptualRoughness, float NoV)
+{
+    //vec3 r = reflect(N);
+    //vec3 Ld = texture(irradianceEnvMap, r).rgb * diffuseColor;
+    //float lod = computeLODFromRoughness(perceptualRoughness);
+    //vec3 Lld = preFilteredReflection(r, perceptualRoughness);
+    //vec2 Ldfg = textureLod(BRDFLut, vec2(NoV, perceptualRoughness), 0.0).xy;
+    //vec3 Lr =  (f0 * Ldfg.x + f90 * Ldfg.y) * Lld;
+    //return Ld + Lr;
+    return vec3(0.0);
+}
+
+vec3 StandardSurfaceShading(vec3 N, vec3 V, vec3 L, vec3 albedo, vec2 metal_rough)
+{
+    vec3 R = reflect(-V,N);
+	vec3 H = normalize(V + L);
+
+    float NoV = abs(dot(N, N)) + 1e-5;
+    //float NoV = clamp(dot(N, V), 0.0, 1.0);
+	float NoL = clamp(dot(N, L), 0.0, 1.0);
+	float NoH = clamp(dot(N, H), 0.0, 1.0);
+    float LoH = clamp(dot(L, H), 0.0, 1.0);
+
+    float roughness = metal_rough.x;
+    float metallic = metal_rough.y;
+
+    roughness = roughness * roughness;
+    vec3 F0 = (1.0 - metallic) + albedo;
+    vec3 diffuse = (1.0 - metallic) * albedo;
+	
+    float D = D_GGX_IMPROVED(roughness, NoH, N, H);
+    vec3 F = fresnelSchlick(LoH, F0);
+    float V_S = V_SmithGGXCorrelated(NoV, NoL, roughness);
+
+    vec3 Fs = (D * V_S) * F;
+    vec3 Fd = diffuse * D_Burley(roughness, LoH, NoL, NoV);
+
+    
+    return vec3(Fd + Fs);
+}
+
+
+float D_Lambert()
+{
+    return 1.0/PI;
+}
+float D_Burley(float roughness, float LoH, float NoL, float NoV)
+{
+    float f90 = 0.5 + 2.0 * roughness * LoH * LoH;
+    float lightScatter = F_Schlick(NoL, 1.0, f90);
+    float viewScatter = F_Schlick(NoV, 1.0, f90);
+    return lightScatter * viewScatter * (1.0 / PI);
+}
+float DistributionGGX(vec3 N, vec3 H, float roughness)
+{
+    float a = roughness*roughness;
+    float a2 = a*a;
+    float NdotH = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH*NdotH;
+
+    float nom   = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+
+    return nom / denom;
+}
+
+// ----------------------------------------------------------------------------
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    float r = (roughness + 1.0);
+    float k = (r*r) / 8.0;
+
+    float nom   = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+
+    return nom / denom;
+}
+// ----------------------------------------------------------------------------
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+
+    return ggx1 * ggx2;
+}
+
+float D_GGX_IMPROVED(float roughness, float NoH, const vec3 n, const vec3 h)
+{
+    vec3 NxH = cross(n, h);
+    float a = NoH * roughness;
+    float k = roughness / (dot(NxH, NxH) + a * a);
+    float d = k * k * (1.0 / PI);
+    return saturateMediump(d);
+}
+float D_GGX(float dotNH, float roughness)
+{
+	float alpha = roughness * roughness;
+	float alpha2 = alpha * alpha;
+	float denom = dotNH * dotNH * (alpha2 - 1.0) + 1.0;
+	return (alpha2)/(PI * denom*denom); 
+}
+
+
+float V_SmithGGXCorrelated(float NoV, float NoL, float roughness)
+{
+    float a2 = roughness * roughness;
+    float GGXV = NoL * sqrt(NoV * NoV * (1.0 - a2) + a2);
+    float GGXL = NoV * sqrt(NoL * NoL * (1.0 - a2) + a2);
+    return 0.5 / (GGXV + GGXL);
+}
+// Geometric Shadowing function --------------------------------------
+float G_SchlicksmithGGX(float dotNL, float dotNV, float roughness)
+{
+	float r = (roughness + 1.0);
+	float k = (r*r) / 8.0;
+	float GL = dotNL / (dotNL * (1.0 - k) + k);
+	float GV = dotNV / (dotNV * (1.0 - k) + k);
+	return GL * GV;
+}
+
+// Fresnel function ----------------------------------------------------
+vec3 F_Schlick(float cosTheta, vec3 F0)
+{
+	return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
+}
+vec3 F_SchlickR(float cosTheta, vec3 F0, float roughness)
+{
+	return F0 + (max(vec3(1.0 - roughness), F0) - F0) * pow(1.0 - cosTheta, 5.0);
+}
--- Black_Key/shaders/lights.glsl
+++ Black_Key/shaders/lights.glsl
@@ -1,4 +1,3 @@
-const float PI = 3.14159265359;
 #define CASCADE_COUNT 4 
 
 
@@ -15,74 +14,6 @@ struct LightGrid{
     uint offset;
     uint count;
 };
-
-float DistributionGGX(vec3 N, vec3 H, float roughness)
-{
-    float a = roughness*roughness;
-    float a2 = a*a;
-    float NdotH = max(dot(N, H), 0.0);
-    float NdotH2 = NdotH*NdotH;
-
-    float nom   = a2;
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
-
-    return nom / denom;
-}
-// ----------------------------------------------------------------------------
-float GeometrySchlickGGX(float NdotV, float roughness)
-{
-    float r = (roughness + 1.0);
-    float k = (r*r) / 8.0;
-
-    float nom   = NdotV;
-    float denom = NdotV * (1.0 - k) + k;
-
-    return nom / denom;
-}
-// ----------------------------------------------------------------------------
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
-{
-    float NdotV = max(dot(N, V), 0.0);
-    float NdotL = max(dot(N, L), 0.0);
-    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
-    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
-
-    return ggx1 * ggx2;
-}
-// ----------------------------------------------------------------------------
-vec3 fresnelSchlick(float cosTheta, vec3 F0)
-{
-    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
-}
-
-float D_GGX(float dotNH, float roughness)
-{
-	float alpha = roughness * roughness;
-	float alpha2 = alpha * alpha;
-	float denom = dotNH * dotNH * (alpha2 - 1.0) + 1.0;
-	return (alpha2)/(PI * denom*denom); 
-}
-
-// Geometric Shadowing function --------------------------------------
-float G_SchlicksmithGGX(float dotNL, float dotNV, float roughness)
-{
-	float r = (roughness + 1.0);
-	float k = (r*r) / 8.0;
-	float GL = dotNL / (dotNL * (1.0 - k) + k);
-	float GV = dotNV / (dotNV * (1.0 - k) + k);
-	return GL * GV;
-}
-
-// Fresnel function ----------------------------------------------------
-vec3 F_Schlick(float cosTheta, vec3 F0)
-{
-	return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
-}
-vec3 F_SchlickR(float cosTheta, vec3 F0, float roughness)
-{
-	return F0 + (max(vec3(1.0 - roughness), F0) - F0) * pow(1.0 - cosTheta, 5.0);
-}
 
 
 void PBR_LOGL()
